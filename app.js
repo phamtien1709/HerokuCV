2e6a27671160a1b29e40080860a7775b78bb1c95
Tien Pham <phamtien1709@gmail.com>
2019-04-11 14:09:23 +0700

    update process env

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.use(express.static(__dirname + '/www'));
 
 // const port = process.env.PORT || config.port;
 
-app.listen(8000, () => {
-  console.log("Your server is online at " + 8000);
+app.listen(process.env.PORT || 8000, function () {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
