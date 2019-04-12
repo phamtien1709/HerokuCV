cf1172c7960fc64e32a5bda2ac4a5ee48826153a
Tien Pham <phamtien1709@gmail.com>
2019-04-12 16:00:50 +0700

    update some

--- www/contact_process.php
+++ www/contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    $to = "phamtien1709@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
